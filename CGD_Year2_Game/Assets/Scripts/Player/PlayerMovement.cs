fcdf2d5972a95397e32d5e995436be9515f3b0e3
CrazyCanineStudios <KennethYorke25@gmail.com>
2017-10-01 15:35:39 +0100

    Added Basic Player Movement
    I've created a new scene named "Level 01" which contains a Player character and a basic environment. The player character was assigned a new C# script  called "PlayerMovement" which allows the player to move with WASD or the arrow keys. It also uses the mouse position to determine where the player is facing.  The Input settings found in Edit - Project Settings - Input were also modified for easier use.

---
+++ CGD_Year2_Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(1, 10)] public float playerSpeed = 6f;
+
+    private Vector3 movement;
+    private Rigidbody playerRigidbody;
+    private int groundMask;
+    private float camRayLength = 100f;
+
+    private void Awake ()
+    {
+        groundMask = LayerMask.GetMask("Ground");
+        playerRigidbody = GetComponent<Rigidbody> ();
+    }
+
+    private void FixedUpdate ()
+    {
+        float leftRight = Input.GetAxisRaw ("LeftRight");
+        float downUp = Input.GetAxisRaw ("DownUp");
+
+        Move (leftRight, downUp);
+        Turning ();
+
+    }
+
+    private void Move (float leftRight, float downUp)
+    {
+        movement.Set (leftRight, 0f, downUp);
+        movement = movement.normalized * playerSpeed * Time.deltaTime;
+        playerRigidbody.MovePosition (transform.position + movement);
+    }
+
+    private void Turning ()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit groundHit;
+
+        if (Physics.Raycast (camRay, out groundHit, camRayLength, groundMask))
+        {
+            Vector3 playerToMouse = groundHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+            playerRigidbody.MoveRotation (newRotation);
+        }
+    }
+}
