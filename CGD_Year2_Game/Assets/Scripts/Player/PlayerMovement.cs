83b8bb4462ae5ef4108280794165bf78a7a28844
CrazyCanineStudios <KennethYorke25@gmail.com>
2017-10-06 16:37:02 +0100

    Added Player Model to Game with Animations
    I added Leon's Player Model to the game. I used a website called Mixamo to auto rig the model and get suitable animations. I set up an Animator Controller in Unity and made some changes to the scripts to get the animations to work. Unfortunately there was an issue with the materials of the player so this will need to be fixed at a later date.

--- CGD_Year2_Game/Assets/Scripts/Player/PlayerMovement.cs
+++ CGD_Year2_Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -12,43 +12,72 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 movement;
     private Rigidbody playerRigidbody;
+    private Animator anim;
     private int groundMask;
     private float camRayLength = 100f;
+    public bool dancing = false;
+    public float dancingTime = 10f;
+    public float atDance;
 
-    private void Awake ()
+    private void Awake()
     {
         groundMask = LayerMask.GetMask("Ground");
-        playerRigidbody = GetComponent<Rigidbody> ();
+        playerRigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
-    private void FixedUpdate ()
+    private void FixedUpdate()
     {
-        float leftRight = Input.GetAxisRaw ("LeftRight");
-        float downUp = Input.GetAxisRaw ("DownUp");
+        float leftRight = Input.GetAxisRaw("LeftRight");
+        float downUp = Input.GetAxisRaw("DownUp");
 
-        Move (leftRight, downUp);
-        Turning ();
+        Animating(leftRight, downUp);
+        Move(leftRight, downUp);
+        Turning();
+        Dancing();
 
+        if (dancing && Time.time > atDance)
+        {
+            dancing = false;
+            playerSpeed = 5;
+            Debug.Log("Dance Finished");
+        }
     }
 
-    private void Move (float leftRight, float downUp)
+
+    private void Move(float leftRight, float downUp)
     {
-        movement.Set (leftRight, 0f, downUp);
+        movement.Set(leftRight, 0f, downUp);
         movement = movement.normalized * playerSpeed * Time.deltaTime;
-        playerRigidbody.MovePosition (transform.position + movement);
+        playerRigidbody.MovePosition(transform.position + movement);
     }
 
-    private void Turning ()
+    private void Turning()
     {
-        Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit groundHit;
 
-        if (Physics.Raycast (camRay, out groundHit, camRayLength, groundMask))
+        if (Physics.Raycast(camRay, out groundHit, camRayLength, groundMask))
         {
             Vector3 playerToMouse = groundHit.point - transform.position;
             playerToMouse.y = 0f;
-            Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
-            playerRigidbody.MoveRotation (newRotation);
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+    private void Animating(float leftRight, float downUp)
+    {
+        bool walking = leftRight != 0f || downUp != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+    private void Dancing()
+    {
+        if ((Input.GetKey(KeyCode.K) && (dancing == false)))
+        {
+            anim.SetTrigger("Dance");
+            playerSpeed = 0;
+            atDance = Time.time + dancingTime;
+            dancing = true;
         }
     }
 }
