360586a72257e2773f572426f04cea1dbb5e5dc6
LeonIrving <31895017+LeonIrving@users.noreply.github.com>
2017-11-24 15:30:55 +0000

    Added ceiling

---
+++ CGD_Year2_Game/Assets/door01Open.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door01Open : MonoBehaviour {
+
+	public GameObject trigger;
+	public GameObject door;
+
+	private float doorClosed;
+	private float doorOpen;
+
+	void Start(){
+		door = GameObject.Find ("prop_door01 (8)");
+		trigger = GameObject.Find ("trigger01");
+		trigger.SetActive (false);
+	}
+
+	void OnTriggerEnter(Collider trigger01)
+	{
+		if (trigger01.tag == "Trigger")
+			trigger.SetActive (true);
+	}
+}
